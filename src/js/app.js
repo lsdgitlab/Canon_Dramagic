ecc2313aa38d495cca61374116e2694456ce3011
Ls-prafulla <prafulla.patil@lsdigital.com>
2022-10-28 12:37:43 +0530

    Header megamenu and enquire popup added on index page. All modified files are- src/css/scss/layout/_header.scss, src/css/scss/pages/_home.scss, src/index.html, src/js/app.js, src/img/canon-logo.png

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,33 @@ import $ from "jquery";
 import lozad from 'lozad';
 
 
-
 import "./page-animation/barba";
 import "./page-animation/gsap";
+
+
+$(document).ready(function(){
+    $('.hamburger-menu').on('click', function() {
+        // alert("hi");
+        $('.hamburger-menu .bar').toggleClass('animate');
+        if($('body').hasClass('open-menu')){
+            $('body').removeClass('open-menu');
+            // $('.menubar').slideToggle();
+            $('.menubar').toggleClass('navOpen');
+        }else{
+            $('body').toggleClass('open-menu');
+            // $('.menubar').slideToggle();
+            $('.menubar').toggleClass('navOpen');
+        }
+    });
+
+    $('.enquire-btn').on('click', function() {
+        $('.enquire').slideDown();
+        $('.enquire-btn').hide();
+    });
+
+    $('.close-btn').on('click', function() {
+        $('.enquire-btn').show();
+        $('.enquire').slideUp();
+    });
+
+});
