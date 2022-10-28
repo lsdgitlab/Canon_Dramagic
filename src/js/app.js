95ba8122ae2a511c8d3f8fe110103868bb0f7520
Ls-prafulla <prafulla.patil@lsdigital.com>
2022-10-28 18:44:34 +0530

    Enquiry form added in enquire popup. modified files are: src/css/scss/abstracts/_variables.scss, src/css/scss/layout/_header.scss, src/index.html, src/js/app.js, src/img/down-arrow.png

--- src/js/app.js
+++ src/js/app.js
@@ -23,13 +23,94 @@ $(document).ready(function(){
     });
 
     $('.enquire-btn').on('click', function() {
-        $('.enquire').slideDown();
+        $('.enquire').slideDown();        
         $('.enquire-btn').hide();
     });
 
     $('.close-btn').on('click', function() {
-        $('.enquire-btn').show();
+        $('.enquire-btn').show(250);
         $('.enquire').slideUp();
     });
 
 });
+
+
+
+let selectInput ,allInput;
+// console.log(selectInput.length);
+
+function removeInvalid(el){
+  if(el.parentElement.nextSibling.nextElementSibling.className === 'invalid'){
+    el.parentElement.nextSibling.nextElementSibling.style.display = 'none';
+    el.classList.remove("error");
+    console.log(el)
+  }
+  if(el.parentElement.getElementsByClassName('label').length){
+    el.parentElement.getElementsByClassName('label')[0].classList.remove("error")
+  }
+}
+
+function submitData(id){
+  console.log("e")
+  console.log(id)
+  let formID = document.getElementById(id)
+  console.log(formID)
+  selectInput = formID.getElementsByTagName("select");
+  allInput = formID.getElementsByClassName("form-control");
+  for (let i = 0; i < selectInput.length; i++) {
+    selectInput[i].addEventListener("change", function() {
+      if (selectInput[i].value) {
+        return selectInput[i].style.color = "#000000";
+      } else {
+        return selectInput[i].style.color = "#888888";
+      }
+    });
+  }
+  
+  
+  for (let indivInput = 0; indivInput < allInput.length; indivInput++) {
+    allInput[indivInput].addEventListener('keyup', function() {
+      removeInvalid(allInput[indivInput])
+    });
+    allInput[indivInput].addEventListener('change', function() {
+      removeInvalid(allInput[indivInput])
+    });
+  }
+  let valid = true;
+  for (let checkIndiv = 0; checkIndiv < allInput.length; checkIndiv++) {
+    if(allInput[checkIndiv].hasAttribute('required')){
+      let input = allInput[checkIndiv]; 
+      if (!input.checkValidity() || !input.value){
+        console.log("input")
+        console.log(input.parentElement.getElementsByClassName('label'))
+        if(input.parentElement.getElementsByClassName('label').length){
+          input.parentElement.getElementsByClassName('label')[0].classList.add("error")
+          // console.log(input.parentElement.getElementsByClassName('label')[0].classList.add("error"))
+        }
+        // console.log(input.parentElement.getElementsByClassName('label').classList.add("error"))
+        valid = false;
+        input.classList.add("error");
+        input.parentElement.nextSibling.nextElementSibling.style.display = 'block';
+        // input.parentElement.nextSibling.nextElementSibling.style.color = 'block';
+        // console.log(valid)
+      }
+    }
+  }
+  if(valid) {
+     console.log("form valid")
+    //  $('#thankYou').popup('show');
+  }else{
+    console.log("form invalid")
+ }
+}
+
+function AllowOnlyNumbers(e) {
+  e = e ? e : window.event;
+  var clipboardData = e.clipboardData ? e.clipboardData : window.clipboardData;
+  var key = e.keyCode ? e.keyCode : e.which ? e.which : e.charCode;
+  var str =
+    e.type && e.type == "paste"
+      ? clipboardData.getData("Text")
+      : String.fromCharCode(key);
+  return /^\d+$/.test(str);
+}
